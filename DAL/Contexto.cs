ea20dc2f9b75b80d656f230e4ceb2ab01a1f130c
Victor Johan <Vicjohan23@gmail.com>
2020-06-15 18:56:52 -0400

    SE AGREGARON LOS SIGUIENTES ELEMENTOS: -Clase Contexto en la carpeta DALL -Se creo la Base de datos Articulos.db -Se agrego la carpeta Migrations

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimerParcial.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimerParcial.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Articulos> Articulos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= DATA\Articulos.db");
+        }
+    }
+}
---
+++ Migrations/20200615225253_Migracion_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrimerParcial.Migrations
+{
+    public partial class Migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(nullable: true),
+                    Existencia = table.Column<int>(nullable: false),
+                    Costo = table.Column<double>(nullable: false),
+                    ValorInventario = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.ProductoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articulos");
+        }
+    }
+}
